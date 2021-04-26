a01ce4fed85e12269577ea5fc6a5915d84c86168
Wurdsmith <wurdsmithing@gmail.com>
2021-04-25 21:29:23 -0500

    commit tree 1be68db2f36fd0fd5895374e87b22d4d4602d416

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Code your solution here
+function findMatching(drivers, string){
+    return drivers.filter(d => d.toLowerCase() === string.toLowerCase());
+
+}
+
+function fuzzyMatch(source, testString) {
+    return source.filter( possibleMatch =>
+      possibleMatch.toLowerCase().indexOf(testString.toLowerCase()) === 0
+    )
+  }
+
+  
+function matchName(source, someName) {
+    return source.filter( record => record.name === someName)
+  }
